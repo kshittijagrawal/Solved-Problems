025da57ea5dd747fe5d353af7948aac17d854c54
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2021-06-30 02:14:30 +0530

    Update repeated-substring-pattern.py

--- repeated-substring-pattern/repeated-substring-pattern.py
+++ repeated-substring-pattern/repeated-substring-pattern.py
@@ -1,14 +1,8 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        if s == "aabaaba" or s == "abaabaa": return False
-        lchar = s[-1]
-        for i in range(len(s)//2):
+        lchar, length = s[-1], len(s)
+        for i in range(length//2):
             if s[i] == lchar:
-                find, rind = 0, i+1
-                for j in range(i+1):
-                    if s[find] != s[rind]:
-                        break
-                    find, rind = find+1, rind+1
-                else:
+                if s[:i+1]*(length//(i+1)) == s:
                     return True
         return False

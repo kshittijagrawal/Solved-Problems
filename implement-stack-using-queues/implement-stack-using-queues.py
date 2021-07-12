e1fd2c01d205982e277f71b83c16a80dc0d342f6
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2021-07-12 23:59:17 +0530

    Time: 48 ms (7.14%), Space: 14.4 MB (41.32%) - LeetHub

---
+++ implement-stack-using-queues/implement-stack-using-queues.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.q1 = deque()
+        self.q2 = deque()
+        self._top = None
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.q1.append(x)
+        self._top = x
+        
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        while len(self.q1) > 1:
+            self._top = self.q1.popleft()
+            self.q2.append(self._top)
+        result = self.q1.popleft()
+        self.q1, self.q2 = self.q2, self.q1
+        return result
+        
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self._top
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return len(self.q1) == 0
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()

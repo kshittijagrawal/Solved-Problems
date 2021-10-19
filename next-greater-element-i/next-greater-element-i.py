629d2aaf2dbdb3d9802cd78d83f0d7818481765f
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2021-10-19 19:13:55 +0530

    Time: 70 ms (39.46%), Space: 14.6 MB (14.67%) - LeetHub

--- next-greater-element-i/next-greater-element-i.py
+++ next-greater-element-i/next-greater-element-i.py
@@ -1,9 +1,10 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        stack, container = collections.deque(), {}
+        container, stack = {}, collections.deque()
         for num in nums2:
-            while stack and stack[-1] < num:
+            while stack and num > stack[-1]:
                 container[stack.pop()] = num
             stack.append(num)
-            
+        
+        print(container)
         return [container.get(num, -1) for num in nums1]

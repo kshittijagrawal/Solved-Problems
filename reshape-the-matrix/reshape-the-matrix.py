396ed2714836c224c1aaf8af06dddd5974002e13
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2021-11-16 00:01:02 +0530

    Time: 92 ms (48.29%), Space: 14.6 MB (95.64%) - LeetHub

--- reshape-the-matrix/reshape-the-matrix.py
+++ reshape-the-matrix/reshape-the-matrix.py
@@ -1,13 +1,14 @@
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-        rowlen, collen = len(mat), len(mat[0])
-        if r*c != rowlen*collen:
-            return mat
-        res, row, col = [[0 for i in range(c)] for j in range(r)], 0, 0
+        m, n = len(mat), len(mat[0])
+        if m*n != r*c: return mat
+        res = [[0 for _ in range(c)] for _ in range(r)]
+        mind, nind = 0, 0
         for i in range(r):
             for j in range(c):
-                res[i][j] = mat[row][col]
-                col += 1
-                if col == collen:
-                    row, col = row+1, 0
+                res[i][j] = mat[mind][nind]
+                nind += 1
+                if nind == n:
+                    mind += 1
+                    nind = 0
         return res

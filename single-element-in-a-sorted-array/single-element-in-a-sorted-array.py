fbc44c8136323a34a33679572ae39f4a0726fcaa
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2021-11-20 16:11:32 +0530

    Time: 72 ms (56.14%), Space: 16.3 MB (75.51%) - LeetHub

--- single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -3,15 +3,14 @@ class Solution:
         lo, hi = 0, len(nums) - 1
         while lo < hi:
             mid = lo + (hi  - lo) // 2
-            if nums[mid - 1] != nums[mid] != nums[mid+1]: return nums[mid]
             if not mid % 2: # even
                 if nums[mid + 1] == nums[mid]:
-                    lo = mid + 1
+                    lo = mid + 2
                 else:
-                    hi = mid - 1
+                    hi = mid
             else: # odd
                 if nums[mid - 1] == nums[mid]:
                     lo = mid + 1
                 else:
-                    hi = mid - 1
+                    hi = mid
         return nums[lo]

c5809bc6fed31256c7763e541ccf5e80025ef51f
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2022-08-29 03:36:13 +0530

    Time: 137 ms (50.94%), Space: 14.2 MB (76.29%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -2,10 +2,9 @@ class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
         
         r, c = len(mat), len(mat[0])
-        formula = 2*min(r, c) + (abs(r - c) - 1) #total number of diagonals
         starti, startj = r - 1, 0
         
-        for _ in range(formula):
+        for _ in range(r + c - 1):
             diag, i, j = [], starti, startj
             
             while i < r and j < c:

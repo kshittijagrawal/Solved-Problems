3c742585ece3f647ed131a9ef69c62196b5985e5
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2022-10-01 21:15:57 +0530

    Time: 35 ms (90.68%), Space: 13.9 MB (23.79%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
@@ -3,6 +3,6 @@ class Solution:
         sub = (int(correct[:2])*60 + int(correct[3:])) - (int(current[:2])*60 + int(current[3:]))
         op, res = (60, 15, 5, 1), 0        
         for o in op:
-            res = res + ((sub) // o)
-            sub = sub % o
+            res += sub // o
+            sub %= o
         return res

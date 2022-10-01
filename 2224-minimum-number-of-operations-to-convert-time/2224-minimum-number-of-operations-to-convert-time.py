cbc45dc0bda211caf36e807bfe725829a5e608ad
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2022-10-01 21:14:06 +0530

    Time: 58 ms (38.27%), Space: 14 MB (23.79%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
@@ -1,11 +1,7 @@
 class Solution:
     def convertTime(self, current: str, correct: str) -> int:
-        initial = int(current[:2])*60 + int(current[3:])
-        final = int(correct[:2])*60 + int(correct[3:])
-        sub = final - initial
-        op = (60, 15, 5, 1)
-        res = 0
-        
+        sub = (int(correct[:2])*60 + int(correct[3:])) - (int(current[:2])*60 + int(current[3:]))
+        op, res = (60, 15, 5, 1), 0        
         for o in op:
             res = res + ((sub) // o)
             sub = sub % o

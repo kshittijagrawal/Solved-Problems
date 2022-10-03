2af4b9d9475cf68adbf2f027c7adc9089329bbb5
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2022-10-03 14:36:27 +0530

    Time: 60 ms (49.01%), Space: 13.9 MB (14.60%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.py
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.py
@@ -4,10 +4,9 @@ class Solution:
         c = {}
         for num in nums:
             if c.get(num):
-                pairs += 1
-                lefts -= 1
+                pairs, lefts = pairs + 1, lefts - 1
                 c[num] -= 1
             else:
-                c[num] = 1
                 lefts += 1
+                c[num] = 1
         return [pairs, lefts]

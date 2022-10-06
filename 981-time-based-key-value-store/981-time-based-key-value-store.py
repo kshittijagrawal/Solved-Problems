f97a84fee350c5a833e3201d4f4306804bafb979
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2022-10-06 21:27:25 +0530

    Time: 837 ms (85.86%), Space: 68.4 MB (95.62%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -1,27 +1,24 @@
 class TimeMap:
 
     def __init__(self):
-        self.structure_value = {}
-        self.structure_time = {}
+        self.s_v = {}
+        self.s_t = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.structure_value.setdefault(key, []).append(value)
-        self.structure_time.setdefault(key, []).append(timestamp)
+        self.s_v.setdefault(key, []).append(value)
+        self.s_t.setdefault(key, []).append(timestamp)
 
     def get(self, key: str, timestamp: int) -> str:
-        lo, hi = 0, len(self.structure_time.get(key, [])) - 1
+        lo, hi = 0, len(self.s_t.get(key, [])) - 1
         mid = 0
         while lo <= hi:
             mid = lo + (hi - lo) // 2
-            if self.structure_time[key][mid] == timestamp:
-                break
-            elif self.structure_time[key][mid] > timestamp:
-                hi = mid - 1
-            else:
-                lo = mid + 1
+            if self.s_t[key][mid] == timestamp: break
+            elif self.s_t[key][mid] > timestamp: hi = mid - 1
+            else: lo = mid + 1
         else:
             mid = lo - 1
-        return self.structure_value[key][mid] if mid >= 0 else ""
+        return self.s_v[key][mid] if mid >= 0 else ""
                 
         
 

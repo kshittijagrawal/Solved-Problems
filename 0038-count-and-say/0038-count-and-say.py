adc08e92bd35a3104f95565174fd478e3608df53
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2022-10-19 02:39:49 +0530

    Time: 111 ms (20.92%), Space: 14.3 MB (8.11%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,30 +1,25 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
         
-        def isSaid(num):
-            currcount, currnum = 0, num % 10
-            pattern = []
-            while num > 0:
-                if num % 10 == currnum:
+        def isSaid(num, curr):
+            if curr == n:
+                return num
+            currcount, currdigit = 1, num[0]
+            res = []
+            
+            for i in range(1, len(num)):
+                if num[i] == currdigit:
                     currcount += 1
                 else:
-                    pattern.append([currcount, currnum])
-                    currcount, currnum = 1, num % 10
-                num //= 10
-            pattern.append([currcount, currnum])
+                    res.append(currcount)
+                    res.append(currdigit)
+                    currcount, currdigit = 1, num[i]
+            res.append(currcount)
+            res.append(currdigit)
             
-            res = []
-            for i in range(len(pattern) - 1, -1, -1):
-                res.append(pattern[i][0])
-                res.append(pattern[i][1])
-            
-            return "".join(list(map(str, res)))
+            return isSaid("".join(list(map(str, res))), curr + 1)
         
-        cas = ""
-        for i in range(1, n + 1):
-            if i == 1:
-                cas = "1"
-            else:
-                cas = isSaid(int(cas))
-        return cas
+        return isSaid("1", 1)
+        
+        
                 

2ff61c958ba4a6c45b7c3b3767af0fd5a8858a74
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2022-11-03 17:27:20 +0530

    Time: 1260 ms (96.19%), Space: 38.2 MB (94.79%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        c = collections.Counter(words)
+        
+        pairs = 0
+        evens = odds = 0
+        for key in c:
+            rev = key[::-1]
+            
+            if key == rev:
+                if c[key] % 2: #odd
+                    odds = max(odds, c[key])
+                    evens += (c[key] - 1)
+                else: #even
+                    evens += c[key]
+                    
+            else:
+                pairs += min(c[key], c.get(rev, 0))
+                c[key] = 0
+        
+        if odds:
+            evens -= (odds - 1)
+        return pairs*4 + evens*2 + odds*2

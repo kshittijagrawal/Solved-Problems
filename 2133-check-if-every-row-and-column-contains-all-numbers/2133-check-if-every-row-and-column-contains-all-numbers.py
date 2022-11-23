dec9f2007ca7df2dcb804a46da25585ee4af7a48
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2022-11-24 01:17:20 +0530

    Time: 937 ms (81.00%), Space: 16.8 MB (5.42%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(n):
             for j in range(n):
                 num = matrix[i][j]
-                if not 1 <= num <= n or (i, num) in r or (j, num) in c:
+                if (i, num) in r or (j, num) in c:
                     return False
                 r.add((i, num))
                 c.add((j, num))

b7f16b9f7ffcc1e1aded414bbb0be4ec19ccf8b8
Kshitij Agrawal <agrawal.kshitij08@gmail.com>
2022-12-01 13:15:29 +0530

    Time: 47 ms (78.56%), Space: 13.9 MB (33.58%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -4,9 +4,9 @@ class Solution:
         first = second = 0
         
         for i, char in enumerate(s):
-            if i < len(s) // 2 and char in c:
-                first += 1
-            elif i >= len(s) // 2 and char in c:
-                second += 1
+            if i < len(s) // 2:
+                first += char in c
+            elif i >= len(s) // 2:
+                second += char in c
         
         return first == second
